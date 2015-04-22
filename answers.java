38bb93e8c08ccc58c7272a37a728d760311b6c32
shahtirth94 <tshah1@ggc.edu>
2015-04-22 13:41:50 -0400

    Update answers.java

--- answers.java
+++ answers.java
@@ -1,44 +1,79 @@
-package test;
-
-/**
- * @author jtran_000
- *
- */
+/**Class: answers
+ * @author John Tran
+ * @version 1.0 
+ * Course : ITEC 3860 Spring 2015 
+ * Written: January 10, 2015 
+ * 
+ * 
+ * This class: holds information about the aID and answers for the other class to use. 
+ * 
+ * Purpose: create qID and answers for the other class to use. 
+ */ 
 public class answers
 {
+	//instance variable
 	private String answers;
+	private int aID;
 
+	//no argument constructor
 	public answers()
 	{
 		answers = "";
+		aID = 0;
 	}
-	
-	/**
-	 * @param answers
-	 */
-	public answers(String answers)
+
+	//argument constructor
+	public answers(int aID, String answers)
 	{
 		this.answers = answers;
+		this.aID = aID;
 	}
 
 	/**
-	 * @return
+	 * Method name: getAnswers()
+	 * @return answers
+	 * description: get the answers information
 	 */
 	public String getAnswers()
 	{
 		return answers;
 	}
 
+	/**
+	 * Method name: setAnswers()
+	 * @param answers
+	 * Description: set the information for answers
+	 */
 	public void setAnswers(String answers)
 	{
 		this.answers = answers;
 	}
 
+	/**
+	 * Method name: getaID
+	 * @return getaID
+	 * Description: get the information from ID
+	 */
+	public int getaID()
+	{
+		return aID;
+	}
+
+	/**
+	 * Method name: setaID
+	 * @param aID
+	 * Description: set the number for aID
+	 */
+	public void setaID(int aID)
+	{
+		this.aID = aID;
+	}
+
 	@Override
 	public String toString()
 	{
-		return "answers: " + answers;
+		return aID + ". " + answers;
 	}
-	
-	
+
+
 }
